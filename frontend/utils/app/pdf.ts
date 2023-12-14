fade34d26a876af14859bb218f58851ce77deac3
LeoTaverne <138439196+LeoTaverne@users.noreply.github.com>
2023-12-14 23:09:50 +0100

    pdf : prise en compte border case
    prise en compte border case pour l'export pdf (c'est toujours aussi moche)

--- frontend/utils/app/pdf.ts
+++ frontend/utils/app/pdf.ts
@@ -9,41 +9,71 @@ import {
 } from '@/types/export';
 import { FolderInterface } from '@/types/folder';
 import { Prompt } from '@/types/prompt';
-import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
+import { PDFDocument, PageSizes, StandardFonts, rgb } from 'pdf-lib'
 
 export async function createPdf(data:LatestExportFormat) {
   const pdfDoc = await PDFDocument.create()
   const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+  const page_size = PageSizes.A4
+
   var all_conversations = data.history
   var all_folder = data.folders
 
-  const page = pdfDoc.addPage()
+  var page = pdfDoc.addPage(page_size)
   var { width, height } = page.getSize()
+  
   const fontSize = 5
+  const base_spacing = 2
+  const marge = 5
+  const bigmarge = 2*marge
 
   for (var conv of all_conversations ){
+    if (height - base_spacing * fontSize <= 0){
+      page = pdfDoc.addPage(page_size)
+      var { width, height } = page.getSize()
+    }
+
     page.drawText("Conversation : " + conv.name, {
-        x: 5,
-        y: height - 4 * fontSize,
+        x: marge,
+        y: height - base_spacing * fontSize,
         size: fontSize,
+        lineHeight:fontSize,
         font: timesRomanFont,
+        maxWidth: width - 2*marge,
       })
-    height = height - 4 * fontSize
+    height = height - base_spacing * fontSize
+
     for (var message of conv.messages){
+        if (height - base_spacing * fontSize <= 0){
+          page = pdfDoc.addPage(page_size)
+          var { width, height } = page.getSize()
+        }
+
         page.drawText(message.role + " : ", {
-            x: 5,
-            y: height - 4 * fontSize,
+            x: bigmarge,
+            y: height - base_spacing * fontSize,
             size: fontSize,
+            lineHeight:fontSize,
             font: timesRomanFont,
+            maxWidth: width - 2*bigmarge,
           })
-        height = height - 4 * fontSize
-        page.drawText(message.content , {
-            x: 5,
-            y: height - 4 * fontSize,
+
+        height = height - base_spacing * fontSize
+
+        if (height - Math.ceil(message.content.length / (width - 2*bigmarge)) * fontSize - base_spacing * fontSize <= 0){
+          page = pdfDoc.addPage(page_size)
+          var { width, height } = page.getSize()
+        }
+
+        page.drawText(message.content, {
+            x: bigmarge,
+            y: height - base_spacing * fontSize,
             size: fontSize,
+            lineHeight:fontSize,
             font: timesRomanFont,
+            maxWidth: width - 2*bigmarge,
           })
-        height = height - 4 * fontSize
+        height = height - Math.ceil(message.content.length / (width - 2*bigmarge)) * fontSize - base_spacing * fontSize
     }
   }
 
