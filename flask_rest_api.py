d13e3caa29051532bed1cc9c5345244f27a50ffc
Léo Alonzo <leo.alonzo@telecomnancy.eu>
2024-01-17 10:44:32 +0100

    fix upload twice

--- flask_rest_api.py
+++ flask_rest_api.py
@@ -25,13 +25,35 @@ def load_assistant_id():
     except FileNotFoundError:
         return None
 
+def get_uploaded_files():
+    try:
+        with open('uploaded_files.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+def save_uploaded_file(file_name, file_id):
+    uploaded_files = get_uploaded_files()
+    uploaded_files[file_name] = file_id
+    with open('uploaded_files.json', 'w') as file:
+        json.dump(uploaded_files, file)
+
 def upload_documents(client, file_paths):
     file_ids = []
+    uploaded_files = get_uploaded_files()
+
     for file_path in file_paths:
-        with open(file_path, 'rb') as file:
-            file_object = client.files.create(file=file, purpose="assistants")
-            file_id = file_object.id
-            file_ids.append(file_id)
+        file_name = file_path.split('/')[-1]
+
+        if file_name in uploaded_files:
+            file_ids.append(uploaded_files[file_name])  # Utiliser l'ID existant
+        else:
+            with open(file_path, 'rb') as file:
+                file_object = client.files.create(file=file, purpose="assistants")
+                file_id = file_object.id
+                file_ids.append(file_id)
+                save_uploaded_file(file_name, file_id)  # Sauvegarder le nouvel ID
+
     return file_ids
 
 def ask_question(client, assistant_id, file_ids, question):
