a7a86e900b78f69180682de1a266afa29c2e073e
LeoTaverne <leo-taverne@hotmail.fr>
2024-01-23 14:18:46 +0100

    missing requirement

--- metric_management.py
+++ metric_management.py
@@ -3,8 +3,8 @@ import tiktoken
 
 class Credit_Manager(ObserverInterface):
     def initialize_metric(self):
-        self.credit_count = 1000
-        self.encoding = tiktoken.get_encoding("cl100k_base")
+        self.credit_count = 0
+        self.encoding = tiktoken.get_encoding("cl100k_base") #Tokenziner de GPT4
         self.credit_per_token_input = 0.00003
         self.credit_per_token_output = 0.00006
 
@@ -14,7 +14,7 @@ class Credit_Manager(ObserverInterface):
 
     def update_metric_output(self,output):
         number_of_token = len(self.encoding.encode(output))
-        self.credit_count -= number_of_token * self.credit_per_token_output
+        self.credit_count += number_of_token * self.credit_per_token_output
 
     def get_metric(self):
         return self.credit_count
